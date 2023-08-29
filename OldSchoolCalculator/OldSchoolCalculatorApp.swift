35d6298e8f3e29509cf6d4d4ef36ba1bcde6f9f1
Tesloboy <meex_v92@mail.ru>
2023-08-29 13:09:17 +0300

    Initial Commit

---
+++ OldSchoolCalculator/OldSchoolCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldSchoolCalculatorApp.swift
+//  OldSchoolCalculator
+//
+//  Created by Viktor Teslenko on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct OldSchoolCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
